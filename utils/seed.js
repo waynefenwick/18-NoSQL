500f1f19f18b9e71686a60942d413101791a8b2e
Wayne <waynefenwick@gmail.com>
2023-07-26 15:43:08 -0500

    added a seed file that now works

--- utils/seed.js
+++ utils/seed.js
@@ -1,51 +1,35 @@
-const connection = require('../config/connection');
-const { User, Thought } = require('../models');
+// To get some data seeded to DB
+const connectToDatabase = require('../config/connection');
+const { User, Thoughts } = require('../models');
 
 const seedData = async () => {
   try {
-    await connection.once('open', async () => {
-      // Seeding logic for users
-      const usersData = [
-        {
-          username: 'user1',
-          email: 'user1@example.com',
-          // other user properties
-        },
-        {
-          username: 'user2',
-          email: 'user2@example.com',
-          // other user properties
-        },
-        // Add more users as needed
-      ];
+    await connectToDatabase(); // Establish the database connection
 
-      const users = await User.create(usersData);
+    // Seeding logic for users
+    const usersData = [
+      {
+        username: 'user1',
+        email: 'user1@example.com',
+        // other user properties
+      },
+    ];
 
-      // Seeding logic for thoughts
-      const thoughtsData = [
-        {
-          thoughtText: 'Thought 1 by user1',
-          username: 'user1', // The username of the user who created this thought
-          // other thought properties
-        },
-        {
-          thoughtText: 'Thought 2 by user1',
-          username: 'user1',
-          // other thought properties
-        },
-        {
-          thoughtText: 'Thought 1 by user2',
-          username: 'user2',
-          // other thought properties
-        },
-        // Add more thoughts as needed
-      ];
+    const users = await User.create(usersData);
 
-      const thoughts = await Thought.create(thoughtsData);
+    // Seeding logic for thoughts
+    const thoughtsData = [
+      {
+        thoughtText: 'Thought 1 by user1',
+        username: 'user1', // The username of the user who created this thought
+        // other thought properties
+      },
+    ];
 
-      console.info('Seeding complete! 🌱');
-      process.exit(0);
-    });
+    const thoughts = await Thoughts.create(thoughtsData);
+
+    console.info('Seeding complete! 🌱');
+    process.exit(0);
   } catch (error) {
     console.error('Error seeding the database:', error);
     process.exit(1);
